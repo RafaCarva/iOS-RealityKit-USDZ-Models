2cf9935c548f00cae229f0f4c2b7066a94085693
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-08-30 10:29:41 -0300

    Insere um carrinho e uma chaleira na tela no "tap";

--- HelloAR/Coordinator.swift
+++ HelloAR/Coordinator.swift
@@ -8,12 +8,14 @@
 import Foundation
 import ARKit
 import RealityKit
-
+import Combine
 
 class Coordinator: NSObject, ARSessionDelegate {
     
     weak var view: ARView?
-   
+    var cancellable: AnyCancellable?
+    
+    
     @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
         
         guard let view = self.view else { return }
@@ -24,8 +26,32 @@ class Coordinator: NSObject, ARSessionDelegate {
         if let result = results.first {
             
             // create anchor entity
+            let anchor = AnchorEntity(raycastResult: result)
             
+            cancellable = ModelEntity.loadAsync(named: "toy_car.usdz")
+                .append(ModelEntity.loadAsync(named: "teapot.usdz"))
+                .collect()
+                .sink { loadCompletion in
+                    if case let .failure(error) = loadCompletion {
+                        print("Unable to load model \(error)")
+                    }
+                    
+                    self.cancellable?.cancel()
+                    
+                } receiveValue: { entities in
+                    
+                    var x: Float = 0.0
+                    
+                    entities.forEach{ entity in
+                                      entity.position = simd_make_float3(x,0,0)
+                                      anchor.addChild(entity)
+                                      x += 0.3
+                    }
+                }
+
             // add anchor to the scene
+            view.scene.addAnchor(anchor)
+            
         }
     }
 }
